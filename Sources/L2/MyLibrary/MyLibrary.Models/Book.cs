8e919c669486ed151d81d7feefd3d72593d1500b
t.teodoru <t.teodoru@levi9.com>
2015-03-14 13:58:56 +0200

    Lab2

---
+++ Sources/L2/MyLibrary/MyLibrary.Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLibrary.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public DateTime PubDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ Sources/L2/MyLibrary/MyLibrary.Repository/BooksRepository.cs
@@ -0,0 +1,22 @@
+﻿using MyLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Repository
+{
+    public class BookRepository : RepositoryBase<BookContext>
+    {
+        public List<Book> GetBooksByTitle(string title)
+        {
+            return DataContext.Books.Where(b => b.Title.Contains(title)).ToList();
+        }
+
+        public Book FindById(int? id)
+        {
+            return DataContext.Books.Find(id);
+        }
+    }
+}
